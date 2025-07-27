eab30c38c5c0ff93aef387013b7d71b8f3dccfd5
Rafael Soares Pinto <rsoaresp@gmail.com>
2025-07-27 10:48:08 -0300

    add my init.lua config for neovim

---
+++ init.lua
@@ -0,0 +1,150 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+--vim.g.python_host_prog = 'home/carrefour/.local/venv/nvim/bin/python'
+vim.opt.number = true
+
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin')
+	Plug 'tpope/vim-sensible'
+	Plug 'nvim-tree/nvim-web-devicons'
+	Plug 'nvim-tree/nvim-tree.lua'
+	Plug 'ray-x/lsp_signature.nvim'
+	Plug 'neovim/nvim-lspconfig'
+	Plug 'hrsh7th/cmp-nvim-lsp'
+	Plug 'hrsh7th/cmp-buffer'
+	Plug 'hrsh7th/cmp-path'
+	Plug 'hrsh7th/cmp-cmdline'
+	Plug 'hrsh7th/nvim-cmp'
+	Plug 'ray-x/go.nvim'
+	Plug "rebelot/kanagawa.nvim"
+	Plug('neoclide/coc.nvim', {['branch'] = 'release'})
+	Plug('averms/black-nvim', {['do'] = ':UpdateRemotePlugins'})
+	Plug('iamcco/markdown-preview.nvim', { ['do'] = 'cd app && npx --yes yarn install' })
+vim.call('plug#end')
+
+
+require('kanagawa').setup({
+    transparent = true,   
+})
+
+vim.cmd("colorscheme kanagawa")
+
+
+require("nvim-tree").setup({
+  sort = {
+    sorter = "case_sensitive",
+  },
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = false,
+    custom = {
+	    ".git",
+	    "__pycache__"
+    }
+  },
+})
+
+
+require'cmp'.setup {
+  sources = {
+    { name = 'nvim_lsp_signature_help' }
+  }
+}
+
+
+
+
+ -- Set up nvim-cmp.
+local cmp = require'cmp'
+
+  cmp.setup({
+    snippet = {
+      -- REQUIRED - you must specify a snippet engine
+      expand = function(args)
+       -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+        -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+         vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+      end,
+    },
+    window = {
+      completion = cmp.config.window.bordered(),
+      documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+      --{ name = 'vsnip' }, -- For vsnip users.
+      -- { name = 'luasnip' }, -- For luasnip users.
+      -- { name = 'ultisnips' }, -- For ultisnips users.
+      -- { name = 'snippy' }, -- For snippy users.
+    }, {
+      { name = 'buffer' },
+    })
+  })
+
+  -- To use git you need to install the plugin petertriho/cmp-git and uncomment lines below
+  -- Set configuration for specific filetype.
+  --[[ cmp.setup.filetype('gitcommit', {
+    sources = cmp.config.sources({
+      { name = 'git' },
+    }, {
+      { name = 'buffer' },
+    })
+ })
+ require("cmp_git").setup() ]]-- 
+
+  -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline({ '/', '?' }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+      { name = 'buffer' }
+    }
+  })
+
+  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+      { name = 'path' }
+    }, {
+      { name = 'cmdline' }
+    }),
+    matching = { disallow_symbol_nonprefix_matching = false }
+  })
+
+  -- Set up lspconfig.
+  local capabilities = require('cmp_nvim_lsp').default_capabilities()
+  require('lspconfig')['pyright'].setup {
+    capabilities = capabilities
+  }
+
+
+require('lspconfig')['bashls'].setup{}
+
+
+
+require('lspconfig')['terraformls'].setup{
+vim.api.nvim_create_autocmd({"BufWritePre"}, {
+  pattern = {"*.tf", "*.tfvars"},
+  callback = function()
+    vim.lsp.buf.format()
+  end,
+})}
+
+vim.keymap.set('n', 'o', ':NvimTreeToggle<CR>', {silent = true})
+vim.keymap.set('n', 'rfmt', ':!ruff format %<CR>', {silent = true})
