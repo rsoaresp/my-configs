0168d7b45c9b31ed8cc3fae2cb98220894898260
Rafael Soares Pinto <rsoaresp@gmail.com>
2025-11-20 21:27:22 -0300

    major updates

--- init.lua
+++ init.lua
@@ -1,12 +1,17 @@
+-- these are configurations required by the nvim-tree plugin
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+vim.opt.termguicolors = true
 
 -- show line numbers on left
 vim.opt.number = true
 
+-- Set global spell option to false initially to disable it for all file types
+vim.opt.spell = false
 vim.opt.modifiable = true
 
-
+-- Creates an autocommand that executes ruff everytime python 
+-- file is saved. It also runs a 'isort' like function
 vim.api.nvim_create_autocmd("BufWritePost", {
   pattern = "*.py",
   callback = function()
@@ -21,30 +26,34 @@ vim.api.nvim_create_autocmd("BufWritePost", {
   end,
 })
 	
-
--- Filetypes to enable spellcheck
-local spell_types = { "text", "plaintex", "typst", "gitcommit", "markdown" }
-
--- Set global spell option to false initially to disable it for all file types
-vim.opt.spell = false
-
--- Create an augroup for spellcheck to group related autocommands
-vim.api.nvim_create_augroup("Spellcheck", { clear = true })
-
--- Create an autocommand to enable spellcheck for specified file types
+-- Create an autocommand to enable spellcheck for text like files.
+-- Do not apply it for source code files
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  group = "Spellcheck", -- Grouping the command for easier management
-  pattern = spell_types, -- Only apply to these file types
+  pattern = { "text", "plaintex", "typst", "gitcommit", "markdown" },   
   callback = function()
-    vim.opt_local.spell = true -- Enable spellcheck for these file types
+    vim.opt_local.spell = true 
   end,
   desc = "Enable spellcheck for defined filetypes", -- Description for clarity
 })
 
 
+-- yank also copies to clipboard;
+vim.api.nvim_create_autocmd("TextYankPost", {
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank { higroup = "IncSearch", timeout = 400 }
+    if vim.v.event.operator == 'y' and vim.v.event.regname == '' then
+            vim.fn.setreg('+', vim.fn.getreg('"'))
+    end
+  end,
+})
+
+
+
 local Plug = vim.fn['plug#']
 
 vim.call('plug#begin')
+	Plug 'ggml-org/llama.vim'
 	Plug 'nvim-treesitter/nvim-treesitter'
 	Plug 'tpope/vim-sensible'
 	Plug 'nvim-tree/nvim-web-devicons'
@@ -57,17 +66,41 @@ vim.call('plug#begin')
 	Plug 'hrsh7th/cmp-cmdline'
 	Plug 'hrsh7th/nvim-cmp'
 	Plug 'ray-x/go.nvim'
-	Plug "rebelot/kanagawa.nvim"
+	Plug 'sainnhe/everforest'
 	Plug('neoclide/coc.nvim', {['branch'] = 'release'})
-	Plug('averms/black-nvim', {['do'] = ':UpdateRemotePlugins'})
 	Plug('iamcco/markdown-preview.nvim', { ['do'] = 'cd app && npx --yes yarn install' })
 vim.call('plug#end')
 
 
-local lspconfig = require('lspconfig')
-lspconfig.gopls.setup{} -- Basic setup for gopls
+-- set neovim theme 
+vim.cmd[[colorscheme everforest]]
 
-require('go').setup()
+
+-- configure treesitter 
+require'nvim-treesitter.configs'.setup {
+  -- A list of parser names, or "all" (the listed parsers MUST always be installed)
+  ensure_installed = { "c", "lua", "vim", "vimdoc", "python", "query", "go", "markdown", "markdown_inline" },
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+
+  -- Automatically install missing parsers when entering buffer
+  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+  auto_install = true,
+
+  highlight = {
+    enable = true,
+
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
+}
+
+
+
 
 local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
 vim.api.nvim_create_autocmd("BufWritePre", {
@@ -78,11 +111,7 @@ vim.api.nvim_create_autocmd("BufWritePre", {
   group = format_sync_grp,
 })
 
-require('kanagawa').setup({
-    transparent = true,   
-})
 
-vim.cmd("colorscheme kanagawa")
 
 
 require("nvim-tree").setup({
@@ -97,6 +126,7 @@ require("nvim-tree").setup({
   },
   filters = {
     dotfiles = false,
+    -- Do not show these files on the tree
     custom = {
 	    ".git",
 	    "__pycache__"
@@ -110,9 +140,6 @@ require'cmp'.setup {
     { name = 'nvim_lsp_signature_help' }
   }
 }
-
-
-
 
  -- Set up nvim-cmp.
 local cmp = require'cmp'
@@ -180,14 +207,21 @@ local cmp = require'cmp'
     matching = { disallow_symbol_nonprefix_matching = false }
   })
 
-  -- Set up lspconfig.
-  local capabilities = require('cmp_nvim_lsp').default_capabilities()
-  require('lspconfig')['pyright'].setup {
-    capabilities = capabilities
-  }
 
 
-require('lspconfig')['bashls'].setup{}
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+vim.lsp.enable('pyright', {
+	settings={
+		capabilities = capabilities
+	},
+})
+
+vim.lsp.enable('bashls', {})
+
+vim.lsp.enable('gopls', {})
+
+
+require('go').setup()
 
 
 
@@ -199,5 +233,4 @@ vim.api.nvim_create_autocmd({"BufWritePre"}, {
   end,
 })}
 
-vim.keymap.set('n', 'o', ':NvimTreeToggle<CR>', {silent = true})
-vim.keymap.set('n', 'rfmt', ':!ruff format %<CR>', {silent = true})
+vim.keymap.set('n', 'o', ':NvimTreeFocus<CR>', {silent = true})
